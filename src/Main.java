ff5c473ccf93948bd04120f171d1ffd20ef32f6e
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-07-29 11:22:51 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner inputUser = new Scanner(System.in);
         String pilihanUser;
 
@@ -22,10 +26,13 @@ public class Main {
 
             switch (pilihanUser) {
                 case "1" -> TampilkanData();
-                case "2" -> System.out.println("Tambah Data");
-                case "3" -> System.out.println("Ubah Data");
-                case "4" -> System.out.println("Hapus Data");
-                case "5" -> System.exit(0);
+                case "2" -> TambahData();
+                case "3" -> UbahData();
+                case "4" -> HapusData();
+                case "5" -> {
+                    Keluar();
+                    return;
+                }
                 default -> System.err.println("Pilihan anda bukan 1-5");
             }
             getYesorNo();
@@ -61,7 +68,54 @@ public class Main {
         }
     }
 
-    private static void TampilkanData() {
+    private static void TampilkanData() throws IOException {
+        FileReader fileInput;
+        BufferedReader bufferInput;
+
+        try {
+            fileInput = new FileReader("../database.txt");
+            bufferInput = new BufferedReader(fileInput);
+        } catch (Exception e) {
+            System.err.println("Database tidak ditemukan");
+            System.err.println("Silahkan tambah data terlebih dahulu");
+            return;
+        }
+
+        System.out.println("\n| No |\t Tahun |\t Penulis              |\t Penerbit             |\t Judul Buku");
+        System.out.println("====================================================================================================");
+        String data = bufferInput.readLine();
+        int nomorData = 0;
+
+        while (data != null) {
+            StringTokenizer stringToken = new StringTokenizer(data, ",");
+            if (stringToken.hasMoreTokens()) {
+                nomorData++;
+                stringToken.nextToken();
+                System.out.printf("| %2d ", nomorData);
+                System.out.printf("|\t %4s  ", stringToken.nextToken());
+                System.out.printf("|\t %-20s  ", stringToken.nextToken());
+                System.out.printf("|\t %-20s  ", stringToken.nextToken());
+                System.out.printf("|\t %s  ", stringToken.nextToken());
+                System.out.println("\n");
+            }
+            data = bufferInput.readLine();
+        }
+        System.out.println("====================================================================================================");
+    }
+
+    private static void TambahData() {
+        System.out.println("Jaga Jarak");
+    }
+
+    private static void UbahData() {
+        System.out.println("Jaga Jarak");
+    }
+
+    private static void HapusData() {
+        System.out.println("Jaga Jarak");
+    }
+
+    private static void Keluar() {
         System.out.println("Jaga Jarak");
     }
 }
